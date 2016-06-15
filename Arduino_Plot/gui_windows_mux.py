784ce7dc65f629ac3efb6290af7935ddeb061a48
lorrainechoi <lorrainechoi@hotmail.com>
2016-06-16 00:08:22 +0100

    Revised LUT

--- Arduino_Plot/gui_windows_mux.py
+++ Arduino_Plot/gui_windows_mux.py
@@ -22,7 +22,6 @@ from midiutil.MidiFile import MIDIFile
 style.use("ggplot")
 total_location = 12
 total_pads = 6
-# no_of_pads = 2
 window_size = 600                                       # sets the duration of data shown
 period = 200                                            # period (in ms) between each message
 
@@ -72,11 +71,8 @@ def init_plot():
         lines["line{0}".format(i)][0].set_xdata(np.arange(time_min, window_size/(1000/period), (float(period)/1000)))
         # lines["line{0}".format(i+1)][0].set_ydata((plot_data["pad{0}".format(i+1)]))
 
-    # a.set_ylim(0,1023)                                        # sets the y axis limits
-    a.set_ylim(0, 35)
+    a.set_ylim(0, 70)
     a.set_xlim(0, window_size/(1000/period))
-    # a.set_title("Arduino Reading")
-    # a.grid(True)
 
     if no_of_pads == 2:
         a.legend([line1, line2], ['pad1', 'pad2'])
@@ -100,14 +96,13 @@ def animate(i):
 
 
     force_mapping = {}
-    force_mapping["force"] = [0, 1.08, 2.13, 3.28, 4.14, 5.04, 5.97, 6.9, 7.76, 8.51, 9.33, 10.04, 10.86, 11.64, 12.35, 13.1, 13.88, 14.48, 15.04, 15.45, 15.75, 16.12, 16.38, 16.68, 16.94, 17.2, 17.46, 17.8, 18.13, 18.47, 18.88, 19.4, 20, 20.52, 21.01, 21.68, 22.39, 23.02, 23.81, 24.55, 25, 25.6, 26.19, 26.94, 27.65, 28.4, 29.18, 29.85, 30.34]
-    force_mapping["reading"] = [6.43, 15.01, 25.74, 36.46, 42.9, 49.33, 57.91, 64.34, 72.92, 83.65, 94.37, 98.66, 113.67, 128.69, 147.99, 169.44, 201.61, 231.64, 263.81, 298.12, 328.15, 368.9, 401.07, 433.24, 471.85, 499.73, 538.34, 572.65, 598.39, 624.13, 647.72, 671.31, 684.18, 694.91, 701.34, 709.92, 718.5, 724.93, 733.51, 742.09, 746.38, 752.82, 759.25, 765.68, 772.12, 780.7, 787.13, 793.57, 802.14]
+    force_mapping["force"] =[0, 7.55, 8.67, 9.71, 10.68, 11.6, 12.47, 13.3, 14.08, 14.82, 15.54, 16.22, 16.87, 17.5, 18.1, 18.68, 19.24, 19.78, 20.3, 20.81, 21.3, 21.77, 22.24, 22.68, 23.12, 23.55, 23.96, 24.36, 24.75, 25.14, 25.51, 25.88, 26.23, 26.58, 26.92, 27.26, 27.58, 27.91, 28.22, 28.53, 28.83, 29.13, 29.42, 29.7, 29.98, 30.26, 30.53, 30.8, 31.06, 31.32, 31.57, 31.82, 32.07, 32.31, 32.55, 32.78, 33.01, 33.24, 33.46, 33.69, 33.9, 34.12, 34.33, 34.54, 34.75, 34.95, 35.15, 35.35, 35.55, 35.74, 35.94, 36.13, 36.31, 36.5, 36.68, 36.86, 37.04, 37.22, 37.39, 37.56, 37.74]
+    force_mapping["reading"] = [0, 150, 161, 172, 183, 194, 205, 216, 227, 238, 249, 260, 271, 282, 293, 304, 315, 326, 337, 348, 359, 370, 381, 392, 403, 414, 425, 436, 447, 458, 469, 480, 491, 502, 513, 524, 535, 546, 557, 568, 579, 590, 601, 612, 623, 634, 645, 656, 667, 678, 689, 700, 711, 722, 733, 744, 755, 766, 777, 788, 799, 810, 821, 832, 843, 854, 865, 876, 887, 898, 909, 920, 931, 942, 953, 964, 975, 986, 997, 1008, 1019]
+
 
     velocity_mapping = {}
-    # velocity_mapping["force"] = np.arange(0, 31)
-    # velocity_mapping["velocity"] = map(int, np.arange(0, 128, 4.233))
-    velocity_mapping["force"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-    velocity_mapping["velocity"] = [0, 4, 8, 13, 17, 21, 25, 30, 34, 38, 42, 47, 51, 55, 59, 64, 68, 72, 76, 80, 85, 89, 93, 97, 102, 106, 110, 114, 119, 123, 127]
+    velocity_mapping["force"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+    velocity_mapping["velocity"] = [0, 3, 6, 10, 13, 16, 19, 22, 25, 29, 32, 35, 38, 41, 44, 48, 51, 54, 57, 60, 64, 67, 70, 73, 76, 79, 83, 86, 89, 92, 95, 98, 102, 105, 108, 111, 114, 117, 121, 124, 127]
 
 
     def force_mapping_func(myNumber):
@@ -129,10 +124,6 @@ def animate(i):
             if len(temp) < total_location:
                 temp = [0]*total_location
 
-            # if no_of_pads < total_pads:
-            #     zero_pad = [0]*(total_pads-no_of_pads)
-            #     temp.extend(zero_pad)
-
         except serial.SerialException:
             print "not connected"
 
@@ -146,13 +137,12 @@ def animate(i):
         if padLocation == -1:
             continue
 
-        # plot_data[colName].append(int(temp[padLocation]))
         plot_data[colName].append(force_mapping_func(int(temp[padLocation])))                                     # append new pressure data to list (0-1023)
         plot_data[colName].pop(0)                                                   # remove first element from list
-        # velocity[i] = int(temp[padLocation])/8
+        print force_mapping_func(int(temp[padLocation]))
         velocity[i] = velocity_mapping_func(force_mapping_func(int(temp[padLocation])))
+        print velocity[i]
         lines["line{0}".format(i+1)][0].set_ydata((plot_data["pad{0}".format(i+1)]))
-        # pad_records["pad{0}".format(i+1)].append(int(temp[padLocation]))
         pad_records["pad{0}".format(i+1)].append(force_mapping_func(int(temp[padLocation])))
 
         if ((plot_data[colName][-1] > 0) & (pad_active[i] == False)):
@@ -168,12 +158,6 @@ def animate(i):
             midi_out.send_message([0x90 + i, pitch[i], 0])                 # Note off
             pad_active[i] = False
 
-    # for i in range(0, total_pads):
-    #     # pad_records["pad{0}".format(i+1)].append = int(temp[i])
-    #     lines["line{0}".format(i+1)][0].set_ydata((plot_data["pad{0}".format(i+1)]))
-
-    # plt.draw()
-    # plt.pause(0.01)
     a.plot()
 
 
@@ -188,27 +172,19 @@ class Application(Tk):
         Tk.__init__(self, *args, **kwargs)
         global ser
         global connected
-        # print str(ser.port) + " " + str(connected)
 
-        # Tk.iconbitmap(self, default = "icon.ico")
         Tk.wm_title(self, "Pressure Touch Music")
 
         container = Frame(self)
         container.pack(side = "top", fill = "both", expand = True)
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
-
-        # container.padLocation = {}
         container.saveFilePath = ''
-
-        # for i in range(1, total_pads+1):
-        #     container.padLocation["pad{0}".format(i)] = IntVar()
 
         self.frames = {}
 
         for F in (InitialisePage, PlotPage):
             frame = F(container, self)
-            # print str(ser.port) + " " + str(connected)
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky = "nesw")
 
@@ -251,7 +227,6 @@ class InitialisePage(Frame) :
             self.padInstrument["pad{0}".format(i)] = StringVar()
             self.padPitch["pad{0}".format(i)] = StringVar()
             self.padOn["pad{0}".format(i)] = BooleanVar()
-            # self.padLocation["pad{0}".format(i)] = StringVar()
             self.padLocation["pad{0}".format(i)] = IntVar()
 
         self.instrument_selection = {}
@@ -308,11 +283,7 @@ class InitialisePage(Frame) :
 
     def create_location_list(self):
         """Create pad location selection list"""
-        # location = [" "]
-        # location.append(str(range(1,total_location+1)))
         location = range(1,total_location+1)
-
-
         for i in range(1, total_pads+1):
             colName = "pad" + str(i)
             pad_location_list = ttk.OptionMenu(self, self.padLocation[colName], " ", *location)
@@ -323,7 +294,6 @@ class InitialisePage(Frame) :
         self.filepath = StringVar()
         self.filename = StringVar()
         self.filename.set(str(strftime("%d-%m-%Y_%H%M", gmtime())) + '.mid')
-        # print self.filename.get()
 
         """Create Browse Button"""
         browse_label = ttk.Label(self, text = "Save csv file in:")
@@ -373,31 +343,23 @@ class InitialisePage(Frame) :
     def connect_arduino_windows(self):
         global ser
         global connected
-        # print ser
-        # print connected
+
         try:
             ser.port = self.comPort.get()
-            # print ser
             ser.open()                                       # sets up serial connection (make sure baud rate is correct - matches Arduino)
-            # print ser
             if not connected:
-                # serin = ser.read()
                 connected = True
-                # print connected
                 connectedMsg = ttk.Label(self, text = "Connected to Arduino!                   ")
                 connectedMsg.grid(row = 12, column = 1, sticky = 'W', columnspan = 3)
         except serial.SerialException:
             connected = False
-            # print connected
             connectedMsg = ttk.Label(self, text = "Failed to connect to Arduino")
             connectedMsg.grid(row = 12, column = 1, sticky = 'W', columnspan = 3)
 
 
     def create_confirm_button(self, master, controller):
         def callback():
-            # master.saveFilePath = self.browse_filepath.get() + "/" + self.saveFileName.get()
             global instrument, pitch, no_of_pads
-
             no_of_pads = 0
 
             for i in range(0, total_pads):
@@ -406,8 +368,7 @@ class InitialisePage(Frame) :
                 instrument[i] = self.instrument_selection["index"][tempIndex]
                 tempIndex = self.pitch_selection["pitch"].index(self.padPitch[colName].get())
                 pitch[i] = self.pitch_selection["index"][tempIndex]
-                # if self.padLocation[colName].get() != " ":
-                #     location[i] = self.padLocation[colName].get()
+
                 try:
                     location[i] = self.padLocation[colName].get()
                 except:
@@ -427,10 +388,6 @@ class PlotPage(Frame):
 
     def __init__(self, master, controller):
 
-        def onClick(event):
-            global pause
-            pause ^= True
-
         global ser, instrument
 
         Frame.__init__(self, master)
@@ -446,11 +403,10 @@ class PlotPage(Frame):
         self.title.pack(pady = 10, padx = 10)
 
 
-        # figure window
+        # figure frame
         canvas = FigureCanvasTkAgg(f, self.graphframe)
         canvas.show()
         canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-        canvas.mpl_connect('button_press_event', onClick)
 
         toolbar = NavigationToolbar2TkAgg(canvas, self.graphframe)
         toolbar.update()
@@ -512,7 +468,6 @@ class PlotPage(Frame):
             binfile = open(filename, 'wb')
             MyMIDI.writeFile(binfile)
             binfile.close()
-            # print "saved"
             tkMessageBox.showinfo(
                 " ",
                 "Saved MIDI file"
@@ -537,7 +492,3 @@ if __name__ == "__main__":
     app.geometry("1600x800")
     ani = animation.FuncAnimation(f, func = animate, init_func = init_plot, interval=50)
     app.mainloop()
-    # global instrument
-    # print no_of_pads
-    # print instrument
-    # print pitch
